cb0bcbbb76a1ff2d2a8c9bdb14cbcb67043be20d
Nishchitha A <nischiashok0@gmail.com>
2025-08-21 12:43:22 +0530

    project

--- component/main.tf
+++ component/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_network_interface" "privateip" {
     name                          = var.name
     subnet_id                     = var.ip_configuration_subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = [azurerm_public_ip.publicip.id]
+    public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
 
