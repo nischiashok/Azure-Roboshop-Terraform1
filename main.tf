d79c247a34d49b8f5cd8c4c8c76279faa9984551
Nishchitha A <nischiashok0@gmail.com>
2025-08-24 16:02:50 +0530

    project

--- main.tf
+++ main.tf
@@ -1,22 +1,26 @@
-module "databases" {
-  for_each                    = var.databases
-  source                      = "./component"
-  ip_configuration_subnet_id  = var.ip_configuration_subnet_id
-  name                        = each.key
-  rg_name                     = var.rg_name
-  storage_image_reference_id  = var.storage_image_reference_id
-  zone_name                   = var.zone_name
-  network_security_group_id   = var.network_security_group_id
-}
+# module "databases" {
+#   for_each                    = var.databases
+#   source                      = "Module/vm"
+#   ip_configuration_subnet_id  = var.ip_configuration_subnet_id
+#   name                        = each.key
+#   rg_name                     = var.rg_name
+#   storage_image_reference_id  = var.storage_image_reference_id
+#   zone_name                   = var.zone_name
+#   network_security_group_id   = var.network_security_group_id
+# }
+#
+# module "applications" {
+#   depends_on                 = [module.databases]
+#   for_each                    =var.applications
+#   source                      = "Module/vm"
+#   ip_configuration_subnet_id  = var.ip_configuration_subnet_id
+#   name                        = each.key
+#   rg_name                     = var.rg_name
+#   storage_image_reference_id  = var.storage_image_reference_id
+#   zone_name                   = var.zone_name
+#   network_security_group_id   = var.network_security_group_id
+# }
 
-module "applications" {
-  depends_on                 = [module.databases]
-  for_each                    =var.applications
-  source                      = "./component"
-  ip_configuration_subnet_id  = var.ip_configuration_subnet_id
-  name                        = each.key
-  rg_name                     = var.rg_name
-  storage_image_reference_id  = var.storage_image_reference_id
-  zone_name                   = var.zone_name
-  network_security_group_id   = var.network_security_group_id
+module "resource-group" {
+  source = "./modules/resource-group"
 }
